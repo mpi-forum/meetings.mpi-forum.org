e41c208e46e426087d7976efec94c207003ae726
Brian Smith <brians@mellanox.com>
2016-07-12 14:05:53 -0400

    Add initial logisitcs info for Sept 2016 meeting in Edinburgh

---
+++ Sep_2016_logistics.php
@@ -0,0 +1,36 @@
+<?
+$topdir = ".";
+include_once("top-nav.php");
+
+$title = "September 21st - 23rd, 2016 Meeting Logistics";
+include_once("$topdir/include/header.php");
+
+?>
+
+<h3><a name=registration>Meeting Registration</a></h3>
+
+
+<p>Advanced registration is required for this meeting.</p>
+<p>A link will be provided shortly. Registration will cost roughly 
+$150US/120GBPand will cover snacks at the meeting and lunch Thursday and Friday.
+</p>
+
+<div align=center><hr width=50%></div>
+
+<h3><a name=location>Meeting Location</a></h3>
+
+<p>The meeting will take place at the John McIntyre Convention Centre Pentland room. This is the same location as the sessions of the EuroMPI 2016 conference and logistics and travel information is listed at <a href="http://www.eurompi2016.ed.ac.uk/venue">http://www.eurompi2016.ed.ac.uk/venue</a>
+</p>
+
+
+<div align=center><hr width=50%></div>
+<h3><a aname=hotel>Hotel</a></h3>
+<p>
+We are looking at availability of the EuroMPI hotels for the week before the EuroMPI conference for the MPI forum. More information will be provided shortly.
+</p>
+
+
+<div align=center><hr width=50%></div>
+
+<?
+include_once("$topdir/include/footer.php");
