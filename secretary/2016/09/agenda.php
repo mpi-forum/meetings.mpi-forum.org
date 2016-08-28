82f0f93050dd5e6ffdb56af50881caf0125e5011
Martin Schulz <schulzm@llnl.gov>
2016-08-27 18:25:27 -0700

    added WGs

--- secretary/2016/09/agenda.php
+++ secretary/2016/09/agenda.php
@@ -19,24 +19,26 @@ agenda_item(" 3:30pm -   5:30pm", "Tools WG");
 agenda_day_end();
 
 agenda_day_start("Thursday, September 22, 2016 - Working Groups and Plenary");
+agenda_item(" 9:00am -   11:00am", "Hybrid WG");
 agenda_item(" 9:00am -   11:00am", "Persistance WG");
 agenda_item(" 11:00am - 11:30am", "Break");
 agenda_item(" 11:30am -   1:00pm", "Persistance WG");
+agenda_item(" 11:30am -   1:00pm", "RMA WG");
 agenda_item(" 1:00pm -   2:00pm", "Lunch");
 agenda_item(" 2:00pm -   3:00pm", "Plenary (see below)");
 agenda_item(" 3:00pm -   3:30pm", "Break");
 agenda_item(" 3:30pm -   5:30pm", "Plenary (see below)");
 agenda_day_end();
 
-agenda_day_start("Friday, September 23, 2016 - Plenary");
+agenda_day_start("Friday, September 23, 2016 - Plenary and Working Groups");
 agenda_item(" 9:00am -   9:30am", "Votes (see below)");
 agenda_item(" 9:30am -   11:00am", "Plenary (see below)");
 agenda_item(" 11:00am - 11:30am", "Break");
 agenda_item(" 11:30am -   1:00pm", "Plenary (see below)");
 agenda_item(" 1:00pm -   2:00pm", "Lunch");
-agenda_item(" 2:00pm -   3:00pm", "Plenary (see below)");
+agenda_item(" 2:00pm -   3:00pm", "WG");
 agenda_item(" 3:00pm -   3:30pm", "Break");
-agenda_item(" 3:30pm -   5:30pm", "Plenary (see below)");
+agenda_item(" 3:30pm -   5:30pm", "WG");
 agenda_day_end();
 
 
