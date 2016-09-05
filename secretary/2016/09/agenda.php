b6fc31b62e2d546141e5535fa2e3a775bde08b21
Martin Schulz <schulzm@llnl.gov>
2016-09-05 16:45:28 -0700

    updated

--- secretary/2016/09/agenda.php
+++ secretary/2016/09/agenda.php
@@ -9,25 +9,26 @@ function ticket($num) {
 }
 
 agenda_day_start("Wednesday, September 21, 2016 - Working Groups");
-agenda_item(" 9:00am -   10:30am", "WG");
+agenda_item(" 9:00am -   10:30am", "Tools / Perf WG");
 agenda_item(" 10:30am - 11:00am", "Break");
-agenda_item(" 11:00am -  12:30pm", "WG");
+agenda_item(" 11:00am -  12:30pm", "Hybrid WG");
+agenda_item(" 11:00am -  12:30pm", "Tools / Perf WG");
 agenda_item(" 12:30pm -   1:30pm", "Lunch");
-agenda_item(" 1:30pm -   3:00pm", "Tools WG");
+agenda_item(" 1:30pm -   3:00pm", "Tools / Perf WG");
+agenda_item(" 1:30pm -   3:00pm", "Persistence WG");
 agenda_item(" 3:00pm -   3:30pm", "Break");
-agenda_item(" 3:30pm -   5:00pm", "Tools WG");
+agenda_item(" 3:30pm -   5:00pm", "Tools / Perf WG");
+agenda_item(" 3:30pm -   5:00pm", "Persistence WG");
 agenda_day_end();
 
 agenda_day_start("Thursday, September 22, 2016 - Working Groups and Plenary");
-agenda_item(" 9:00am -   10:30am", "Hybrid WG");
-agenda_item(" 9:00am -   10:30am", "Persistance WG");
+agenda_item(" 9:00am -   10:30am", "Plenary (see below)");
 agenda_item(" 10:30am - 11:00am", "Break");
-agenda_item(" 11:00am -  12:30pm", "Persistance WG");
-agenda_item(" 11:00am -  12:30pm", "RMA WG");
+agenda_item(" 11:00am -  12:30pm", "Plenary (see below)");
 agenda_item(" 12:30pm -   1:30pm", "Lunch");
 agenda_item(" 1:30pm -   3:00pm", "Plenary (see below)");
 agenda_item(" 3:00pm -   3:30pm", "Break");
-agenda_item(" 3:30pm -   5:00pm", "Plenary (see below)");
+agenda_item(" 3:30pm -   5:00pm", "Tools / Debugging WG");
 agenda_day_end();
 
 agenda_day_start("Friday, September 23, 2016 - Plenary and Working Groups");
@@ -46,9 +47,9 @@ agenda_item(" 9:30am -   10:30am", "Plenary (see below)");
 agenda_item(" 10:30am - 11:00am", "Break");
 agenda_item(" 11:00am -   12:30pm", "Plenary (see below)");
 agenda_item(" 12:30pm -   1:30pm", "Lunch");
-agenda_item(" 1:30pm -   3:00pm", "WG");
+agenda_item(" 1:30pm -   3:00pm", "RMA WG");
 agenda_item(" 3:00pm -   3:30pm", "Break");
-agenda_item(" 3:30pm -   5:00pm", "WG");
+agenda_item(" 3:30pm -   5:00pm", "RMA WG");
 agenda_day_end();
 
 
@@ -56,7 +57,9 @@ agenda_plenary_start("Plenary Items to be Discussed",0);
 plenary_item("Welcome","Introductions","All",0);
 plenary_item("Update","WG status","All WG chairs or their proxies",0);
 plenary_item("Change Reading","<a href=\"https://github.com/mpi-forum/mpi-issues/issues/26\">#26</a>: Deprecate point-to-point send cancel","Dan",0);
-plenary_item("Discussion","Persistence Plenary","Tony",0);
+plenary_item("Reading","Persistent Collectives","Tony",0);
+plenary_item("Update","Tools: The MPI_T Event Interface","Marc-Andre",0);
+plenary_item("","Towards a New Debugger Interface","Kathryn",0);
 agenda_plenary_end();
 
 include_once("$topdir/include/footer.php");
