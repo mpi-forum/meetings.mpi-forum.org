58a9c61bf5ae970341926db842400b22411a507b
Martin Schulz <schulzm@llnl.gov>
2016-09-10 18:29:07 -0700

    added to agenda added logistics for Dec

---
+++ Dec_2016_logistics.php
@@ -0,0 +1,49 @@
+<?
+$topdir = ".";
+include_once("top-nav.php");
+
+$title = "December 5-8, 2016   Meeting Logistics";
+include_once("$topdir/include/header.php");
+?>
+
+<p align="right">
+
+<ul>
+<li> <a href="#registration">Meeting registration</a></li>
+<li> <a href="#location">Meeting location</a></li>
+<li> <a href="#cost">Meeting cost</a></li>
+<li> <a href="#hotel-block">Hotel room block</a></li>
+</ul>
+
+<div align="center"><hr width=50%></div>
+
+<h3><a name=registration>Meeting Registration</a></h3>
+
+
+<p>Advanced registration is required for this meeting - Registration is available later.</p>
+
+
+<p>The registration covers snacks at the meeting on Monday, Tuesday
+Wednesday, and Thursday lunch on Tuesday and Wednesday, and the meeting logistics.  </p>
+
+
+<p>Advanced registration is required for this meeting so that IBM
+can process their visitor processing procedure.</p>
+
+<div align=center><hr width=50%></div>
+
+<h3><a name=location>Meeting Location</a></h3>
+
+<p>The meeting will take
+place at the IBM facility 1177 S Belt Line Rd, Coppell, TX 75019</a>.</p>
+
+<div align="center"><hr width=50%></div>
+
+<h3><a name=hotel-block>Hotel Room Block</a></h3>
+
+TBD.
+
+</p>
+
+<?
+include_once("$topdir/include/footer.php");
